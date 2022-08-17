93543ab74dd64b93bfc9351569c24cb500319df1
Son Bui Ngoc <sonbn@hiworld.com.vn>
2022-08-17 17:06:46 +0700

    update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+// import './assets/head.scss'
+// import './assets/style.scss'
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ var all = glob.sync('./src/**/*.scss'),
     head = []
 
 all.forEach(function(e, i){
-    console.log(e)
+    // console.log(e)
     if(e.match((/.*\.head\.scss/))){
         head.push(path.resolve(__dirname, e))
     }else{
@@ -20,58 +20,69 @@ all.forEach(function(e, i){
 })
 
 const htmlPlugin = () => {
-  return {
-    name: 'html-transform',
-    transformIndexHtml: async function(html, context){
-      console.log(html, context, process.argv)
-      // let css = await fs.readFile(path.resolve(__dirname, 'dist/assets/'), 'utf8')
+    return {
+        name: 'html-transform',
+        transformIndexHtml: async function(html, {bundle}){
+            // console.log(context, process.argv)
 
-      html = html.replace(
-          /<title>(.*?)<\/title>/,
-          `<title>bigson</title>`)
+            html = html.replace(
+                        /<title>(.*?)<\/title>/,
+                        `<title>bigson</title>`)
 
-      return html
+            if(process.argv.includes('build')){
+                let bundles = Object.values(bundle),
+                head = bundles.find(x => x.name == 'src/assets/head.scss')
+                if(head){
+                    // let css = await fs.readFile(path.resolve(__dirname, 'dist/assets/'), 'utf8')
+                    html = html.replace(
+                      '</head>',
+                      `<style>\t${head.source}\t\t</style>\n</head>`
+                    )
+                }
+            }
+
+            return html
+        }
     }
-  }
 }
 
-console.log(head, style, import.meta, path.resolve(__dirname, "src/views/abc.head.scss"))
+console.log(head, style, import.meta, path.resolve(__dirname, "src/assets/head.scss"), 'xxx')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), htmlPlugin ()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    manifest: true,
-    // lib:{
-    //   head : {
-    //     entry : path.resolve(__dirname, "src/views/abc.head.scss"),
-    //     fileName : 'head_css'
-    //   },
-    //   style : {
-    //     entry: path.resolve(__dirname, "src/views/abc.scss"),
-    //     fileName : 'style_css'
-    //   }
-    // },
-    rollupOptions: {
-      input:{
-        main: path.resolve(__dirname, 'index.html'),
-        head_css: head,
-        style_css: style,
-      }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "@/views/_variables.scss";
-        `
-      }
-    }
-  },
+    plugins: [vue(), htmlPlugin ()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        // manifest: true,
+        // lib:{
+        //   head : {
+        //     entry : path.resolve(__dirname, "src/views/abc.head.scss"),
+        //     fileName : 'head_css'
+        //   },
+        //   style : {
+        //     entry: path.resolve(__dirname, "src/views/abc.scss"),
+        //     fileName : 'style_css'
+        //   }
+        // },
+        // rollupOptions: {
+        //     input:{
+        //         main: path.resolve(__dirname, 'index.html'),
+        //         head_css: path.resolve(__dirname, "src/assets/head.scss"),
+        //         style_css: path.resolve(__dirname, "src/assets/style.scss"),
+        //     }
+        // }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                @import "@/views/_variables.scss";
+                `
+            }
+        }
+    },
 })
